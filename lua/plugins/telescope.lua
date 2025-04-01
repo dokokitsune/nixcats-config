6fcc89470e76475a8132ba203c0410f76a217055
Weston Wood <stakin101@gmail.com>
2025-04-01 15:33:51 -0700

    added fzf and ui-select to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,19 +16,26 @@ return {
     "telescope.nvim",
     dep_of = { "plenary.nvim", "nvim-web-devicons" },
     event = "DeferredUIEnter",
-    on_load = function(name)
+    load = function(name)
       vim.cmd.packadd(name)
       vim.cmd.packadd("telescope-fzf-native.nvim")
+      vim.cmd.packadd("telescope-ui-select.nvim")
     end,
+    on_require = { "telescope" },
     after = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-      require("plugins.telescope-ui")
-
-      require("telescope").load_extension("fzf")
+      -- require("plugins.telescope-ui")
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        },
+      }
+      pcall(require("telescope").load_extension, "ui-select")
+      pcall(require("telescope").load_extension, "fzf")
     end,
   },
 }
